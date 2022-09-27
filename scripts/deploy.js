97eaa0b7f4000568bbb57572b1ed9911a19d8136
mehmet yilmaz <mehmetyilmaz@MacBookmehmet.local>
2022-09-27 23:09:48 +0200

    update contract + deploy on goerli network

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,19 +2,22 @@ const hre = require("hardhat");
 
 async function main() {  
 
-    // We get the contract to deploy  
     const horoscopeNFT = await hre.ethers.getContractFactory("horoscopeNFT");
     const hscp = await horoscopeNFT.deploy();   
     await hscp.deployed();
-    
+	
+	//since we are testing, you should mention your own Eth wallet address
+    const myAddress="0x42FD6663aFB5A5ed0F4C5426743204bCE6d76E6F";
     console.log("horoscopeNFT deployed to:", hscp.address);   
+
+    let txn = await hscp.mintNFT(myAddress, 'aquarius');
+    await txn.wait();
+
  } 
- 
-// We recommend this pattern to be able to use async/await
-//everywhere and properly handle errors.
+
 main()
     .then(() => process.exit(0))  
     .catch((error) => {    
     console.error(error);
-    process.exit(1);  
-});
+    process.exit(1); 
+ });
